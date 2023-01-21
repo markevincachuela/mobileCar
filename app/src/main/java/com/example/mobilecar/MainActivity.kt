d49fe23a88fef815299488b7ebf6180c037c7051
narding30 <32611773+narding30@users.noreply.github.com>
2023-01-21 20:11:02 +0800

    Update copyright

--- app/src/main/java/com/example/mobilecar/MainActivity.kt
+++ app/src/main/java/com/example/mobilecar/MainActivity.kt
@@ -6,11 +6,13 @@ package com.example.mobilecar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobilecar.ui.theme.MobileCarTheme
@@ -34,7 +36,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)
